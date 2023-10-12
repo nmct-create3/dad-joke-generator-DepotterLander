7eecea42cb28539600f6c5f2b2dee6c61603a5f5
DepotterLander <lander.depotter@student.howest.be>
2023-10-12 09:37:37 +0200

    Het werkt

--- script/app.js
+++ script/app.js
@@ -1,5 +1,26 @@
-const init = function () {
-    console.log('script loaded');
+// const init = function () {
+//     console.log('script loaded');
+// }
+
+// init();
+
+function fetchJoke() {
+    fetch('https://icanhazdadjoke.com/', {
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Display the joke in the joke-container
+        const jokeContainer = document.getElementById('joke-container');
+        jokeContainer.innerHTML = data.joke;
+    })
+    .catch(error => {
+        console.error('Error fetching joke:', error);
+    });
 }
 
-init();
+// Add a click event listener to the button
+const button = document.querySelector('.c-button');
+button.addEventListener('click', fetchJoke);
